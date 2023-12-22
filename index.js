9aad69dede32b1b3171afa3d58d8c49b7dc24f4e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-12-23 00:01:35 +0530

    Add : Modules Folder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 //////////////////////////////Files/////////////////////////////
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
@@ -43,20 +44,6 @@ const tempProduct = fs.readFileSync(
   "utf-8"
 );
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 const productDataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
